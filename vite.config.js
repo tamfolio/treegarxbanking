2520570ef25400257c7c643b69aff7fa05904e42
unknown <tamiloreolaniyi@gmail.com>
2026-04-08 22:07:38 +0100

    security updates

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import subresourceIntegrity from 'vite-plugin-subresource-integrity'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    subresourceIntegrity()
   ]
 })
