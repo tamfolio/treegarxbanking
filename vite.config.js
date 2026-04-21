173846b52b80266937a8dfffdca28b34f26253bb
unknown <tamiloreolaniyi@gmail.com>
2026-04-21 04:03:45 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    subresourceIntegrity()
-  ]
+    subresourceIntegrity({
+      ignoreSources: [/^https?:\/\//], // ignore all external CDN URLs
+    }),
+  ],
 })
