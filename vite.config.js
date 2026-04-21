ff156e9851bc90816c8a12a9d310cac377a76adb
unknown <tamiloreolaniyi@gmail.com>
2026-04-21 04:07:20 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import subresourceIntegrity from 'vite-plugin-subresource-integrity'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    subresourceIntegrity({
-      ignoreSources: [/^https?:\/\//], // ignore all external CDN URLs
-    }),
   ],
 })
