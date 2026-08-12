dd4d2276041ffa781a2d4ed12735400070c21be2
unknown <tamiloreolaniyi@gmail.com>
2026-08-12 10:49:59 +0100

    Remove malicious code injected into vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-});
+})
